fc6bfa92d6d9edacc2412b1eb5b7ea76542f0770
Pitius <ballaipeterlorant@gmail.com>
2025-02-28 18:23:09 +0100

    Fixed Video Author not being able to access edit video page

--- src/pages/EditVideoPage.jsx
+++ src/pages/EditVideoPage.jsx
@@ -20,13 +20,6 @@ const EditVideoPage = () => {
 
         setVideoData(videoResponse.data);
 
-        if(!user){
-          navigate("/login")
-        }
-        else if(user.id !== videoData.userId){
-          navigate(`/video/${id}`)
-        }
-        
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -35,10 +28,29 @@ const EditVideoPage = () => {
     fetchData();
   }, [id]);
 
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+      return;
+    }
+
+    if (videoData.userId && user.id !== videoData.userId) {
+      navigate(`/video/${id}`);
+      return;
+    }
+
+    if (videoData.title) {
+      document.title = `Edit ${videoData.title}`;
+    }
+
+  }, [user, videoData, navigate, id]);
+
   return (
     <div>
       <h1>{videoData.title}</h1>
       <p>{videoData.description}</p>
+      <p>{videoData.userId}</p>
+      <p>{user.id}</p>
       {/* Add more properties as needed */}
     </div>
   );
