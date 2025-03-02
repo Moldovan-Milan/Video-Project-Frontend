5b695af700f7e8ec90a24a52e3890fe3ca56d66e
Pitius <ballaipeterlorant@gmail.com>
2025-03-02 12:39:34 +0100

    Fixed upload video, because it checked for a jwtToken in sessionStorage, which was missing after logging in via remember me

--- src/pages/UploadVideo.jsx
+++ src/pages/UploadVideo.jsx
@@ -25,7 +25,7 @@ const UploadVideo = () => {
 
   const { user } = useContext(UserContext)
 
-  if (sessionStorage.getItem("jwtToken") === null || !user) {
+  if (!user) {
     return (
       <div className="container mx-auto p-4">
         <h1 className="text-center text-2xl font-bold mb-4">
