df0abb100352eb5f210ed4f6ef8577828f09a08e
Pitius <ballaipeterlorant@gmail.com>
2025-03-07 22:29:23 +0100

    Added ViewHistoryPage to AppRoutes and Navbar Component

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -99,6 +99,14 @@ export default function NavbarComponent() {
                     Chats
                   </Link>
                 </li>
+                <li className="nav-item mb-2">
+                  <Link
+                    to="/watch-history"
+                    className="nav-link px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Watch History
+                  </Link>
+                </li>
               </>
             )}
             <li className="nav-item mb-2">
