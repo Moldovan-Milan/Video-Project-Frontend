23b28fa218d0c56f5fcbecf2fc70b60a6b57a89a
KGabor5 <gabo.kocsis05@gmail.com>
2025-03-21 18:18:19 +0100

    Created popup dialog for a user to edit their own username, fixed display error on the UserAccount page for users with a transparent background avatar image (no functionality has been implemented yet)

--- src/components/UserAccountDetailsPanel.jsx
+++ src/components/UserAccountDetailsPanel.jsx
@@ -1,15 +1,57 @@
 import ImageEditor from "./ImageEditor";
 import "../styles/UserAccountDetailsPanel.scss";
+import { FaPencil } from "react-icons/fa6";
+import { useState } from "react";
 
 export default function UserAccountDetailsPanel({user})
 {
+
+    const [editing, setEditing] = useState(null);
+    let editDialog = null
+    
+    if (editing!=null) {
+        editDialog = <div className="editBg">
+            <div className="editUsernameWindow">
+            <h1>Editing <span className="titleEditUname">{user.username}</span>'s username</h1>
+            <div>
+            <label>New username: </label>
+            <input value={editing} onChange={e => setEditing(prev => {
+                prev = e.target.value
+                return prev
+            })} />
+            </div>
+            <div>
+            <button onClick={() => {
+                if(editing!="")
+                {
+                    user.username=editing
+                    setEditing(null)
+                }
+                else
+                {
+                    window.alert("Type in new username!")
+                }        
+            }} className="editUsernameSave">Save</button>
+            <button onClick={() => setEditing(null)} className="editUsernameCancel">Cancel</button>
+            </div>
+        </div>
+        </div>
+    }
+
+    
+
     return(
+        
         <div className="DivDetailsPanel">
+            {editDialog}
             <h2>Account datails</h2>
             <hr className="AccLine"></hr>
         <div className="DivAccDetails">
         <label>Channel name: </label>
+        <div className="UserAccNameEditDiv">
         <p className="AccInfo">{user.username}</p>
+        <button className="editUnsernameBtn" onClick={()=>setEditing(user.username)}><FaPencil className="m-1"/></button>
+        </div>
         </div>
         <div className="DivAccDetails">
         <label>Email address: </label>
