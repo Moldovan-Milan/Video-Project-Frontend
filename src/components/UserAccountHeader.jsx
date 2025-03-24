6c64f171715ef0b597063794badcfcae762294c9
Moldován Milán János <moldovanmilan2005@gmail.com>
2025-03-24 21:51:18 +0100

    Fixed missing banner error

--- src/components/UserAccountHeader.jsx
+++ src/components/UserAccountHeader.jsx
@@ -1,18 +1,34 @@
 import { FaUserPlus } from "react-icons/fa";
 import "../styles/UserAccountHeader.scss";
-import banner from "../assets/teszt_banner.png";
+import banner from "../assets/default_banner.png";
+import { useEffect, useState } from "react";
 
-export default function UserAccountHeader({user})
-{
-    const BASE_URL = import.meta.env.VITE_BASE_URL;
+export default function UserAccountHeader({ user }) {
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const [bannerUrl, setBannerUrl] = useState(null);
 
-    return(
+  useEffect(() => {
+    try {
+      if (user.userTheme.bannerId) {
+        setBannerUrl(`${BASE_URL}/api/user/banner/${user.userTheme.bannerId}`);
+      } else {
+        setBannerUrl(banner);
+      }
+    } catch {
+      setBannerUrl(banner);
+    }
+  }, []);
+
+  return (
     <>
-    {/* {<img src={`${BASE_URL}/api/user/banner/${user.userTheme.bannerId}`} className="UserAccBanner"></img>} */}
-    <img src={user.avatar} className="UserAccAvatar"/>
-    <h1 className="UserAccUsername text-center">{user.username}</h1>
-    <div className="subscribersLabel UserAccSubs">
-    <FaUserPlus className="m-1"/><p>Your subscribers: {user.followers}</p>
-    </div>
-    </>)
+      <img src={bannerUrl} className="UserAccBanner"></img>
+
+      <img src={user.avatar} className="UserAccAvatar" />
+      <h1 className="UserAccUsername text-center">{user.username}</h1>
+      <div className="subscribersLabel UserAccSubs">
+        <FaUserPlus className="m-1" />
+        <p>Your subscribers: {user.followers}</p>
+      </div>
+    </>
+  );
 }
