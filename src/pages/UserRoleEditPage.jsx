de6d8c1e5f9a7f5e0ed9ec3c0383316f0df567b4
Pitius <ballaipeterlorant@gmail.com>
2025-04-04 22:05:39 +0200

    Made getRoles.js be able to get the roles of any users

--- src/pages/UserRoleEditPage.jsx
+++ src/pages/UserRoleEditPage.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 
 const UserRoleEditPage = () => {
+  const[pageNumber, setPageNumber] = useState(1)
+  const pageSize = 30
+  useEffect(() => {
+    const getUsers = async () => {
+        try {
+            const response = await axios.get(
+              `api/user?pageSize=${pageSize}&pageNumber=${pageNumber}`
+            );
+            const { users, hasMore } = response.data;
+    
+            setVideos((prevUsers) => {
+                const filteredUsers = users.filter(
+                    (newUser) => !prevUsers.some((user) => user.id === newUser.id)
+                );
+                return [...prevUsers, ...filteredUsers];
+            });
+            setHasMore(hasMore);
+          } catch (error) {
+            console.error("Error fetching users:", error);
+          }
+    }
+  }, [pageNumber])
+
   return (
     <div>UserRoleEditPage</div>
   )
