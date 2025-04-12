075dcc550bbfc685e6b38e67077632a256953999
Pitius <ballaipeterlorant@gmail.com>
2025-04-12 14:20:38 +0200

    fixed formatDuration to work with videos longer than an hour

--- src/functions/formatDuration.js
+++ src/functions/formatDuration.js
@@ -1,6 +1,19 @@
 const formatDuration = (duration) => {
-    const parts = duration.split(":");
-    return parts.length === 3 ? parts.slice(1).join(":") : duration;
-  };
+  const parts = duration.split(":").map(Number);
+
+  if (parts.length === 3) {
+    const [hours, minutes, seconds] = parts;
+    if (hours === 0) {
+      return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    } else {
+      return `${String(hours)}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    }
+  } else if (parts.length === 2) {
+    const [minutes, seconds] = parts;
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  }
+
+  return duration;
+};
 
 export default formatDuration;
