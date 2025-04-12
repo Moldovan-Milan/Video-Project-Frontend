330c4a595e25f6dba5f1a6e4c7693afe89b85438
Pitius <ballaipeterlorant@gmail.com>
2025-04-12 17:09:03 +0200

    Added a link to the user's avatar, which redirects to their profile

--- src/components/VerificationRequestItem.jsx
+++ src/components/VerificationRequestItem.jsx
@@ -1,6 +1,7 @@
 import React, { forwardRef } from 'react';
 import VerificationRequestHandler from './VerificationRequestHandler';
 import "../styles/VerificationRequestItem.scss";
+import { Link } from 'react-router-dom';
 
 const VerificationRequestItem = forwardRef(({ user, onRequestProcessed }, ref) => {
   const BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -8,8 +9,11 @@ const VerificationRequestItem = forwardRef(({ user, onRequestProcessed }, ref) =
   const AVATAR_PATH = import.meta.env.VITE_AVATAR_PATH;
   return (
     <div ref={ref} className='verification-request-container'>
-      <img src={`${CLOUDFLARE_PATH}/${AVATAR_PATH}/${user.avatar.path}.${user.avatar.extension}`}
-       style={{ borderRadius: 100, width: 112, height:112 }} />
+      <Link to={`/profile/${user.id}`}>
+        <img src={`${CLOUDFLARE_PATH}/${AVATAR_PATH}/${user.avatar.path}.${user.avatar.extension}`}
+        style={{ borderRadius: 100, width: 112, height:112 }}
+        title={user.userName} />
+      </Link>
       <VerificationRequestHandler user={user} onRequestProcessed={onRequestProcessed} />
     </div>
   );
