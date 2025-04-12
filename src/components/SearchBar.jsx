1b563c6c28906536bfc59a3b01f351e4db27474f
Pitius <ballaipeterlorant@gmail.com>
2025-04-12 17:45:04 +0200

    Made it so you can search by hitting enter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,28 +5,40 @@ import "../styles/SearchBar.scss";
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  const [searchstr, setSearchstr] = useState("");
+  const navigate = useNavigate();
 
-  const[searchstr,setSearchstr]=useState("");
-  const navigate=useNavigate();
+  const handleSearch = () => {
+    if (searchstr) {
+      navigate(`/search?key=${searchstr}`);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
 
   return (
     <nav className="p-4">
       <div className="container mx-auto flex">
-          <input
-            className="form-input w-full px-4 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 search-input"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-            style={{ color: "black" }}
-            onChange={(e)=>{setSearchstr(e.target.value)}}
-            value={searchstr}
-          ></input>
-          <button
-            className="text-white px-4 py-2 focus:outline-none focus:ring-2 search-btn"
-            onClick={searchstr?()=>navigate(`/search?key=${searchstr}`):()=>window.alert("Type in a word to search for!")}
-          >
-            <FaSearch />
-          </button>
+        <input
+          className="form-input w-full px-4 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 search-input"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          style={{ color: "black" }}
+          onChange={(e) => { setSearchstr(e.target.value) }}
+          onKeyDown={handleKeyDown}
+          value={searchstr}
+        ></input>
+        <button
+          className="text-white px-4 py-2 focus:outline-none focus:ring-2 search-btn"
+          onClick={handleSearch}
+        >
+          <FaSearch />
+        </button>
       </div>
     </nav>
   );
