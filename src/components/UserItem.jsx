557f746f767485cb7f6e19f62434de87e48c3f10
Pitius <ballaipeterlorant@gmail.com>
2025-04-12 16:54:37 +0200

    Improved styling and design of userItems

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -5,7 +5,7 @@ const UserItem = ({user}) => {
   const CLOUDFLARE_PATH = import.meta.env.VITE_PUBLIC_CLOUDFLARE_URL;
   const AVATAR_PATH = import.meta.env.VITE_AVATAR_PATH;
   return (
-    <div className='user-item-container'>
+    <div className='user-item-container' title={user.userName}>
         <img src={`${CLOUDFLARE_PATH}/${AVATAR_PATH}/${user.avatar.path}.${user.avatar.extension}`}/>
         <p>{user.userName}</p>
     </div>
