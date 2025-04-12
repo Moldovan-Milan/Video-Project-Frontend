6921855835b761f6e94cb0703f51ba1c748329d4
Pitius <ballaipeterlorant@gmail.com>
2025-04-12 16:59:33 +0200

    Changed size of Avatar on VerificationRequestItems

--- src/components/VerificationRequestItem.jsx
+++ src/components/VerificationRequestItem.jsx
@@ -4,9 +4,12 @@ import "../styles/VerificationRequestItem.scss";
 
 const VerificationRequestItem = forwardRef(({ user, onRequestProcessed }, ref) => {
   const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const CLOUDFLARE_PATH = import.meta.env.VITE_PUBLIC_CLOUDFLARE_URL;
+  const AVATAR_PATH = import.meta.env.VITE_AVATAR_PATH;
   return (
     <div ref={ref} className='verification-request-container'>
-      <img src={`${BASE_URL}/api/User/avatar/${user.avatarId}`} style={{ borderRadius: 100 }} />
+      <img src={`${CLOUDFLARE_PATH}/${AVATAR_PATH}/${user.avatar.path}.${user.avatar.extension}`}
+       style={{ borderRadius: 100, width: 112, height:112 }} />
       <VerificationRequestHandler user={user} onRequestProcessed={onRequestProcessed} />
     </div>
   );
